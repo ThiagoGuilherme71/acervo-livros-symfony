59ab22441304846fbdebe196302babfa6e02b19e
ThiagoGuilherme71 <thiagoguilherme.barbosaa@gmail.com>
2026-09-09 22:19:40 -0300

    feat(tests): adição dos testes unitários para AutorService, LivroService e AssuntoService

---
+++ tests/Controller/LivroControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+
+class LivroControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private EntityManagerInterface $entityManager;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->entityManager = $this->client->getContainer()->get('doctrine')->getManager();
+
+        $conexao = $this->entityManager->getConnection();
+        $conexao->executeStatement('DELETE FROM livro_assunto');
+        $conexao->executeStatement('DELETE FROM livro_autor');
+        $conexao->executeStatement('DELETE FROM livro');
+    }
+
+    public function testCriarLivroSemAutorDeveExibirMensagemDeErro(): void
+    {
+        $crawler = $this->client->request('GET', '/livros/novo');
+        $this->assertResponseIsSuccessful();
+
+        $form = $crawler->selectButton('Salvar livro')->form([
+            'livro[titulo]' => 'Dom Casmurro',
+            'livro[editora]' => 'Editora X',
+            'livro[edicao]' => 1,
+            'livro[anoPublicacao]' => '1899',
+            'livro[valor]' => '39,90',
+        ]);
+
+        $this->client->submit($form);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('.alert-danger', 'pelo menos um autor');
+    }
+}
---
+++ tests/Service/AssuntoServiceTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Assunto;
+use App\Entity\Livro;
+use App\Exception\AssuntoDuplicadoException;
+use App\Exception\AssuntoPossuiLivroVinculadoException;
+use App\Repository\AssuntoRepository;
+use App\Service\AssuntoService;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+
+class AssuntoServiceTest extends TestCase
+{
+    private EntityManagerInterface $entityManager;
+    private AssuntoRepository $assuntoRepository;
+    private AssuntoService $assuntoService;
+
+    protected function setUp(): void
+    {
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->assuntoRepository = $this->createMock(AssuntoRepository::class);
+
+        $this->assuntoService = new AssuntoService(
+            $this->entityManager,
+            $this->assuntoRepository
+        );
+    }
+
+    public function testCriarComDescricaoUnicaDevePersistirERetornarAssunto(): void
+    {
+        $this->assuntoRepository
+            ->method('findOneBy')
+            ->with(['descricao' => 'Ficção'])
+            ->willReturn(null);
+
+        $this->entityManager->expects($this->once())->method('persist');
+        $this->entityManager->expects($this->once())->method('flush');
+
+        $assunto = $this->assuntoService->criar('Ficção');
+
+        $this->assertSame('Ficção', $assunto->getDescricao());
+    }
+
+    public function testCriarComDescricaoDuplicadaDeveLancarExcecao(): void
+    {
+        $assuntoExistente = new Assunto();
+        $assuntoExistente->setDescricao('Ficção');
+
+        $this->assuntoRepository
+            ->method('findOneBy')
+            ->willReturn($assuntoExistente);
+
+        $this->entityManager->expects($this->never())->method('persist');
+
+        $this->expectException(AssuntoDuplicadoException::class);
+
+        $this->assuntoService->criar('Ficção');
+    }
+
+    public function testAtualizarSemAlterarDescricaoNaoDeveLancarExcecao(): void
+    {
+        $assunto = new Assunto();
+        $assunto->setDescricao('Ficção');
+
+        $this->assuntoRepository
+            ->method('findOneBy')
+            ->willReturn($assunto);
+
+        $this->entityManager->expects($this->once())->method('flush');
+
+        $resultado = $this->assuntoService->atualizar($assunto, 'Ficção');
+
+        $this->assertSame('Ficção', $resultado->getDescricao());
+    }
+
+    public function testAtualizarComDescricaoJaUsadaPorOutroAssuntoDeveLancarExcecao(): void
+    {
+        $assuntoAtual = new Assunto();
+        $assuntoAtual->setDescricao('Ficção');
+
+        $outroAssunto = new Assunto();
+        $outroAssunto->setDescricao('Terror');
+
+        $this->assuntoRepository
+            ->method('findOneBy')
+            ->willReturn($outroAssunto);
+
+        $this->expectException(AssuntoDuplicadoException::class);
+
+        $this->assuntoService->atualizar($assuntoAtual, 'Terror');
+    }
+
+    public function testExcluirAssuntoSemLivroDeveRemover(): void
+    {
+        $assunto = new Assunto();
+        $assunto->setDescricao('Ficção');
+
+        $this->entityManager->expects($this->once())->method('remove');
+        $this->entityManager->expects($this->once())->method('flush');
+
+        $this->assuntoService->excluir($assunto);
+    }
+
+    public function testExcluirAssuntoComLivroVinculadoDeveLancarExcecao(): void
+    {
+        $assunto = new Assunto();
+        $assunto->setDescricao('Ficção');
+
+        $livro = new Livro();
+        $assunto->addLivro($livro);
+
+        $this->entityManager->expects($this->never())->method('remove');
+
+        $this->expectException(AssuntoPossuiLivroVinculadoException::class);
+
+        $this->assuntoService->excluir($assunto);
+    }
+}
